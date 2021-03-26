e6fefd384d7c70f7071eea5b49c5c18f7d088406
SteveTsio <13-13115@saeinstitute.edu>
2021-03-26 02:17:59 +0200

    semi stable

--- js/burger.js
+++ js/burger.js
@@ -1,14 +1,12 @@
-const mainMenu = document.querySelector('.main-nav');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
-
-function show(){
-    mainMenu.style.display='flex';
-    mainMenu.style.top='0';
-}
-function close(){
-    mainMenu.style.top = '-100%';
+function burger(){
+    
+    var menu = document.querySelector('main-nav')
+    if (menu.style.display ==! "flex") 
+    {
+        menu.style.display = "flex";
+     } 
+     else
+    {
+        menu.style.display = "none";
+     }
 }
