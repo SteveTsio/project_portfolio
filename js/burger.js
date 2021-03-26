43dc8a0a1863b28df52a0139dac1bf6cbcd248a1
SteveTsio <72525330+SteveTsio@users.noreply.github.com>
2021-03-26 21:03:58 +0200

    console log for debug

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,7 @@
 function burger(){
     
     var menu = document.getElementsByClassName('main-nav')
+    console.log(menu);
     if (menu.style.display ==! "flex") 
     {
         menu.style.display = "flex";
