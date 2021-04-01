e7f47132c51d52b69a9d7a8127647a33d78e7bf0
SteveTsio <13-13115@saeinstitute.edu>
2021-04-01 21:40:38 +0300

    final bug fix and dead code deletion

--- js/burger.js
+++ js/burger.js
@@ -1,13 +1,23 @@
-function burger(){
-    
-    var menu = document.getElementsByClassName('main-nav')
-
-    if (menu[0].style.display != "flex") 
-    {
+let menu = document.getElementsByClassName('main-nav');
+let width;
+let height;
+window.onresize = window.onload = function() {
+    width = this.innerWidth;
+    height = this.innerHeight;
+    if(width > 550){
         menu[0].style.display = "flex";
-     } 
-     else
-    {
-        menu[0].style.display = "none";
-     }
+    }
+}
+
+function burger(){
+    if(width <= 550){
+        if (menu[0].style.display != "flex") 
+        {
+            menu[0].style.display = "flex";
+        } 
+         else
+        {
+            menu[0].style.display = "none";
+        }
+    }
 }
